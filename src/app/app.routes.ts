404d5d4c9c9e8fe1cc33063cd13f924629466769
br4hm <haehnlein.alexander@gmx.de>
2024-12-19 18:47:24 +0100

    implement poke-lost | poke-card | header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'pokedex', pathMatch: 'full'},
+  {
+    path:'pokedex',
+    loadComponent: () =>
+    import('./modules/poke-list/poke-list.component').then(
+      m => m.PokeListComponent),
+  }
+
+];
---
+++ src/app/modules/poke-list/poke-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from '../../core/components/header/header.component';
+import {PokeCardComponent} from './poke-card/poke-card.component';
+
+@Component({
+  selector: 'app-poke-list',
+  imports: [
+    HeaderComponent,
+    PokeCardComponent
+  ],
+  templateUrl: './poke-list.component.html',
+  styleUrl: './poke-list.component.scss'
+})
+export class PokeListComponent {
+  cards = Array(20).fill(0);
+}
