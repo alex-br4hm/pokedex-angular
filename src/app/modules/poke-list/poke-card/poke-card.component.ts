cc728ce2f160a024fa0495e616929ecf57f7c325
br4hm <haehnlein.alexander@gmx.de>
2024-12-28 20:05:15 +0100

    adjustments/styling poke-img & poke-info

--- src/app/modules/poke-list/poke-card/poke-card.component.ts
+++ src/app/modules/poke-list/poke-card/poke-card.component.ts
@@ -1,13 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {
   MatCard,
-  MatCardActions,
   MatCardContent,
   MatCardHeader,
   MatCardImage, MatCardSubtitle,
   MatCardTitle
 } from '@angular/material/card';
-import {MatButton} from '@angular/material/button';
 import {ApiService} from '../../../core/services/api.service';
 import {
   CustomLoadingSpinnerComponent
@@ -21,14 +19,12 @@ import {PokeNumberPipePipe} from '../../../shared/utils/poke-number-pipe.pipe';
     MatCard,
     MatCardHeader,
     MatCardContent,
-    MatCardActions,
-    MatButton,
     MatCardImage,
     MatCardTitle,
     MatCardSubtitle,
-    CustomLoadingSpinnerComponent,
     NgOptimizedImage,
-    PokeNumberPipePipe
+    PokeNumberPipePipe,
+    CustomLoadingSpinnerComponent
   ],
   templateUrl: './poke-card.component.html',
   styleUrl: './poke-card.component.scss'
@@ -37,20 +33,71 @@ export class PokeCardComponent implements OnInit {
   @Input() pokeData: any;
   @Input() index!: number;
   data:any;
+  germanName!: string;
+  flavorText!: string;
+  germanTypes!: string[];
+  imgLoaded:boolean = false;
 
   constructor(private apiService: ApiService) {
   }
 
   ngOnInit() {
-    this.apiService.getSinglePokemon(this.index+1).subscribe({
+    this.apiService.getSinglePokemon(this.index + 1).subscribe({
       next: data => {
         this.data = data;
-          console.log(this.data);
+        this.germanTypes = this.getGermanTypesFromArray(this.data.types);
       },
       error: error => {console.log(error);
       }});
 
+    this.apiService.getGermanInfo(this.index + 1).subscribe({
+      next: data => {
+        this.germanName = data.names.find(
+          (name: { language: { name: string; }; }) => name.language.name === 'de')?.name;
+
+        const germanFlavor = data.flavor_text_entries.find(
+          (entry: { language: { name: string; }; }) => entry.language.name === 'de');
+        this.flavorText = germanFlavor ? germanFlavor.flavor_text : 'Keine Beschreibung vorhanden.';
+      }
+    })
+
 
   }
+
+  getGermanTypesFromArray(typesArray: { slot: number, type: { name: string, url: string } }[]): string[] {
+    return typesArray.map(typeObj => this.getGermanTypeName(typeObj.type.name));
+  }
+
+  getGermanTypeName(type: string): string {
+    const types = [
+      { english: 'normal', german: 'Normal' },
+      { english: 'fire', german: 'Feuer' },
+      { english: 'water', german: 'Wasser' },
+      { english: 'electric', german: 'Elektro' },
+      { english: 'grass', german: 'Pflanze' },
+      { english: 'flying', german: 'Flug' },
+      { english: 'bug', german: 'Käfer' },
+      { english: 'poison', german: 'Gift' },
+      { english: 'rock', german: 'Gestein' },
+      { english: 'ground', german: 'Boden' },
+      { english: 'fighting', german: 'Kämpfer' },
+      { english: 'ice', german: 'Eis' },
+      { english: 'psychic', german: 'Psycho' },
+      { english: 'ghost', german: 'Geist' },
+      { english: 'dragon', german: 'Drache' },
+      { english: 'fairy', german: 'Fee' },
+      { english: 'dark', german: 'Unlicht' },
+      { english: 'steel', german: 'Stahl' },
+    ];
+
+    const typeObject = types.find((t) => t.english === type);
+
+    if (typeObject) {
+      return typeObject.german;
+    } else {
+      return 'Unbekannter Typ'; // Rückgabe, falls der Typ nicht gefunden wird
+    }
+  }
+
 
 }
