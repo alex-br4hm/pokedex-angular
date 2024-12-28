5ba8bf543e7ddbc17ab7e81d5a54355e9b2d3707
br4hm <haehnlein.alexander@gmx.de>
2024-12-28 14:31:14 +0100

    add animation provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {provideHttpClient} from '@angular/common/http';
+import {provideNoopAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideNoopAnimations(),
     provideAnimationsAsync(),
     provideHttpClient()
   ]
