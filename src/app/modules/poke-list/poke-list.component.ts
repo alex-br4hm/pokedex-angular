78ff7fdc8d1f389f7a469fea238fa51d1a35f960
br4hm <haehnlein.alexander@gmx.de>
2024-12-28 20:06:15 +0100

    cleanup

--- src/app/modules/poke-list/poke-list.component.ts
+++ src/app/modules/poke-list/poke-list.component.ts
@@ -5,20 +5,22 @@ import {ApiService} from '../../core/services/api.service';
 import {Pokemon, PokemonData} from '../../core/models/pokemon';
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
 import {CustomLoadingSpinnerComponent} from '../../shared/ui/custom-loading-spinner/custom-loading-spinner.component';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
+import {CdkFixedSizeVirtualScroll, CdkVirtualForOf, CdkVirtualScrollViewport} from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-poke-list',
   imports: [
     HeaderComponent,
     PokeCardComponent,
-    MatProgressSpinner,
-    CustomLoadingSpinnerComponent,
+    CustomLoadingSpinnerComponent
   ],
   templateUrl: './poke-list.component.html',
   styleUrl: './poke-list.component.scss'
 })
 export class PokeListComponent implements OnInit {
   pokeList!: any;
+  pokeListGer!: any;
 
 
   constructor(private apiService: ApiService) {
