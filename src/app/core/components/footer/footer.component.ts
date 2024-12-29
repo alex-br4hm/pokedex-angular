4302e73c950a83b62d3137635623e75c85af301b
br4hm <haehnlein.alexander@gmx.de>
2024-12-29 23:02:59 +0100

    implement footer

---
+++ src/app/core/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {MatTooltip} from '@angular/material/tooltip';
+
+@Component({
+  selector: 'app-footer',
+  imports: [
+    MatIcon,
+    MatTooltip
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
