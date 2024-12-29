56ddad727f78a4cb3ed25778832f51e1464269d9
br4hm <haehnlein.alexander@gmx.de>
2024-12-29 20:00:27 +0100

    implement routing with parameter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,26 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'pokedex', pathMatch: 'full'},
+  {
+    path: '',
+    redirectTo: 'pokedex',
+    pathMatch: 'full'},
   {
     path:'pokedex',
     loadComponent: () =>
     import('./modules/poke-list/poke-list.component').then(
-      m => m.PokeListComponent),
+      m => m.PokeListComponent
+    ),
+    children: [
+      {
+        path: 'detail-view/:game_index',
+        loadComponent: () =>
+          import('./modules/poke-detail-view/poke-detail-view.component').then(
+            m => m.PokeDetailViewComponent
+          )
+      }
+
+    ]
   }
 
 ];
