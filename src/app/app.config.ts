2e659173679b1ef5bc91994d76572ccf565334d3
br4hm <haehnlein.alexander@gmx.de>
2025-01-09 22:32:59 +0100

    fix routing for FTP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideRouter, withHashLocation} from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {provideHttpClient} from '@angular/common/http';
@@ -14,7 +14,7 @@ import { initializeApp } from "@angular/fire/app"
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideNoopAnimations(),
     provideAnimationsAsync(),
     provideHttpClient(),
