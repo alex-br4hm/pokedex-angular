2da5c5e5bb00ea39da77372dbab3ab2fd442ff60
br4hm <haehnlein.alexander@gmx.de>
2025-01-12 14:56:15 +0100

    implement get filter selection

--- src/app/modules/poke-list/search-filter-bar/filter-sidebar/filter-sidebar.component.ts
+++ src/app/modules/poke-list/search-filter-bar/filter-sidebar/filter-sidebar.component.ts
@@ -1,11 +1,84 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {MatSlider, MatSliderRangeThumb} from '@angular/material/slider';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {FormBuilder, FormControl, ReactiveFormsModule} from '@angular/forms';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatOption, MatSelect} from '@angular/material/select';
+import {MatChip} from '@angular/material/chips';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-filter-sidebar',
-  imports: [],
+  imports: [
+    MatSlider,
+    MatSliderRangeThumb,
+    MatCheckbox,
+    ReactiveFormsModule,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    MatLabel,
+    MatChip,
+    MatButton
+  ],
   templateUrl: './filter-sidebar.component.html',
   styleUrl: './filter-sidebar.component.scss'
 })
 export class FilterSidebarComponent {
+  // selectedTypes = new FormControl('');
+  _formBuilder = inject(FormBuilder);
 
+  filterSelections = this._formBuilder.group({
+    types: this._formBuilder.group({
+      Normal: true,
+      Feuer: true,
+      Wasser: true,
+      Elektro: true,
+      Pflanze: true,
+      Flug: true,
+      Käfer: true,
+      Gift: true,
+      Gestein: true,
+      Boden: true,
+      Kämpfer: true,
+      Eis: true,
+      Psycho: true,
+      Geist: true,
+      Drache: true,
+      Fee: true,
+      Unlicht: true,
+      Stahl: true,
+    }),
+    weightRange: '',
+    heightRange: ''
+  })
+
+
+
+  getFilterSelection() {
+    const selection = this.filterSelections.value;
+    console.log(selection);
+  }
+
+
+  typeList = [
+    'Normal',
+    'Feuer',
+    'Wasser',
+    'Elektro',
+    'Pflanze',
+    'Flug',
+    'Käfer',
+    'Gift',
+    'Gestein',
+    'Boden',
+    'Kämpfer',
+    'Eis',
+    'Psycho',
+    'Geist',
+    'Drache',
+    'Fee',
+    'Unlicht',
+    'Stahl'
+  ];
 }
