88409bc5d12d62a8d5ee9eb2c030030e9b9812f7
br4hm <haehnlein.alexander@gmx.de>
2025-01-12 15:57:02 +0100

    implement filter functionality

--- src/app/modules/poke-list/search-filter-bar/filter-sidebar/filter-sidebar.component.ts
+++ src/app/modules/poke-list/search-filter-bar/filter-sidebar/filter-sidebar.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MatSlider, MatSliderRangeThumb} from '@angular/material/slider';
 import {MatCheckbox} from '@angular/material/checkbox';
 import {FormBuilder, FormControl, ReactiveFormsModule} from '@angular/forms';
@@ -6,6 +6,11 @@ import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatOption, MatSelect} from '@angular/material/select';
 import {MatChip} from '@angular/material/chips';
 import {MatButton} from '@angular/material/button';
+import {JsonPipe} from '@angular/common';
+import {PokeWeightPipe} from '../../../../shared/utils/poke-weight.pipe';
+import {PokeHeightPipe} from '../../../../shared/utils/poke-height.pipe';
+import {Pokemon} from '../../../../core/models/pokemon';
+import {PokeDataService} from '../../../../core/services/poke-data.service';
 
 @Component({
   selector: 'app-filter-sidebar',
@@ -19,14 +24,26 @@ import {MatButton} from '@angular/material/button';
     MatOption,
     MatLabel,
     MatChip,
-    MatButton
+    MatButton,
+    JsonPipe,
+    PokeWeightPipe,
+    PokeHeightPipe
   ],
   templateUrl: './filter-sidebar.component.html',
   styleUrl: './filter-sidebar.component.scss'
 })
-export class FilterSidebarComponent {
+export class FilterSidebarComponent implements OnInit {
   // selectedTypes = new FormControl('');
+  pokeList!: Pokemon[] | undefined;
+  minHeight: number = 0;
+  maxHeight: number = 0;
+  minWeight: number = 0;
+  maxWeight: number = 0;
+
   _formBuilder = inject(FormBuilder);
+
+  constructor(private pokeDataService: PokeDataService) {
+  }
 
   filterSelections = this._formBuilder.group({
     types: this._formBuilder.group({
@@ -49,11 +66,33 @@ export class FilterSidebarComponent {
       Unlicht: true,
       Stahl: true,
     }),
-    weightRange: '',
-    heightRange: ''
+    weightRange: this._formBuilder.group({
+      startValue: 0,
+      endValue: 200,
+    }),
+    heightRange: this._formBuilder.group({
+      startValue: 0,
+      endValue: 200,
+    }),
   })
 
+  ngOnInit() {
+    this.pokeList = this.pokeDataService.$pokemonList;
 
+    if (this.pokeList) {
+      const weights = this.pokeList.map(p => p.weight);
+      const heights = this.pokeList.map(p => p.height);
+
+      this.minWeight = Math.min(...weights);
+      this.maxWeight = Math.max(...weights);
+
+      this.minHeight = Math.min(...heights);
+      this.maxHeight = Math.max(...heights);
+    }
+
+
+
+  }
 
   getFilterSelection() {
     const selection = this.filterSelections.value;
@@ -81,4 +120,5 @@ export class FilterSidebarComponent {
     'Unlicht',
     'Stahl'
   ];
+
 }
