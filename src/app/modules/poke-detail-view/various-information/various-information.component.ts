36abb343af873080c0b8e67142aab1da643fbdcc
br4hm <haehnlein.alexander@gmx.de>
2025-01-22 19:56:02 +0100

    implement various information

--- src/app/modules/poke-detail-view/various-information/various-information.component.ts
+++ src/app/modules/poke-detail-view/various-information/various-information.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Pokemon, Stats} from '../../../core/models/pokemon';
+import {PokeHeightPipe} from '../../../shared/utils/poke-height.pipe';
+import {PokeWeightPipe} from '../../../shared/utils/poke-weight.pipe';
+import {PokeNumberPipePipe} from '../../../shared/utils/poke-number-pipe.pipe';
 
 @Component({
   selector: 'app-various-information',
-  imports: [],
+  imports: [
+    PokeHeightPipe,
+    PokeWeightPipe,
+    PokeNumberPipePipe
+  ],
   templateUrl: './various-information.component.html',
   styleUrl: './various-information.component.scss'
 })
 export class VariousInformationComponent {
+  @Input() pokemon: Pokemon | undefined;
+
 
 }
