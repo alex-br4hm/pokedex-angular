b1bf22d22a74a101fce307ec96e5df1c5431dab0
br4hm <haehnlein.alexander@gmx.de>
2025-01-22 22:42:49 +0100

    implement stats (chart)

--- src/app/modules/poke-detail-view/stats/stats.component.ts
+++ src/app/modules/poke-detail-view/stats/stats.component.ts
@@ -1,10 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 
 import {
   CustomLoadingSpinnerComponent
 } from '../../../shared/ui/custom-loading-spinner/custom-loading-spinner.component';
 import { NgxChartsModule} from '@swimlane/ngx-charts';
 import {BaseChartDirective} from 'ng2-charts';
+import {Chart, ChartConfiguration, registerables} from 'chart.js';
+import {Stats} from '../../../core/models/pokemon';
 
 
 
@@ -18,8 +20,46 @@ import {BaseChartDirective} from 'ng2-charts';
   templateUrl: './stats.component.html',
   styleUrl: './stats.component.scss'
 })
-export class StatsComponent {
-  @Input() stats: { name: string; value: number }[] = [];
+export class StatsComponent implements OnInit, AfterViewInit {
+  @Input() stats: Stats[] = [];
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    Chart.register(...registerables);
+  }
+
+  ngAfterViewInit() {
+    this.loadChart();
+  }
+
+  loadChart() {
+    const ctx = document.getElementById('radarChart') as HTMLCanvasElement;
+    const labels = ['HP', 'Angriff', 'Verteidig.', 'Sp. Angr.', 'Sp. Verteid.', 'Geschwindigk.'];
+    const data = this.stats.map(stat => stat.value);
+
+    new Chart(ctx, {
+      type: 'radar',
+      data: { labels, datasets: [{ label: 'Stats', data }] },
+      options: {
+        responsive: true,
+        scales: {
+          r: {
+            ticks: { display: false, stepSize: 30 },
+            pointLabels: { color: 'white' },
+            angleLines: { display: true, color: 'rgba(255, 255, 255, 0.75)' },
+            grid: { color: 'rgba(255, 255, 255, 0.75)' },
+            suggestedMin: 0,
+            suggestedMax: 120,
+
+          },
+        },
+        plugins: {
+          legend: { display: false },
+        },
+      },
+    });
+  }
 
 }
