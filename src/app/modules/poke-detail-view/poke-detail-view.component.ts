7b449be6ebf45ab054a7fd6e2d536c25b50ed28b
br4hm <haehnlein.alexander@gmx.de>
2025-01-25 17:10:05 +0100

    refactor navigation

--- src/app/modules/poke-detail-view/poke-detail-view.component.ts
+++ src/app/modules/poke-detail-view/poke-detail-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, DestroyRef, inject, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {ActivatedRoute, Router, RouterLink} from '@angular/router';
 import {MatIcon} from '@angular/material/icon';
 import {PokeDataService} from "../../core/services/poke-data.service";
@@ -7,14 +7,13 @@ import {Pokemon} from '../../core/models/pokemon';
 import {CustomLoadingSpinnerComponent} from '../../shared/ui/custom-loading-spinner/custom-loading-spinner.component';
 import {NgOptimizedImage} from '@angular/common';
 import {MatTab, MatTabGroup} from '@angular/material/tabs';
-import { Subscription} from 'rxjs';
 import {StatsComponent} from './stats/stats.component';
 import {FirebaseService} from '../../core/services/firebase.service';
 import {PokeHeightPipe} from '../../shared/utils/poke-height.pipe';
 import {PokeWeightPipe} from '../../shared/utils/poke-weight.pipe';
 import {VariousInformationComponent} from './various-information/various-information.component';
-import {MatDivider} from '@angular/material/divider';
 import {EvolutionChainComponent} from './evolution-chain/evolution-chain.component';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-poke-detail-view',
@@ -35,14 +34,12 @@ import {EvolutionChainComponent} from './evolution-chain/evolution-chain.compone
   templateUrl: './poke-detail-view.component.html',
   styleUrl: './poke-detail-view.component.scss'
 })
-export class PokeDetailViewComponent implements OnInit, OnDestroy {
+export class PokeDetailViewComponent implements OnInit {
   pokemon: Pokemon | undefined;
   pokeList: Pokemon[] = [];
   game_index: number = 1;
-  isLoading: boolean = true;
-
-  private routeSub: Subscription | null = null;
-  private firebaseSub: Subscription | null = null;
+  name: string = '';
+  destroyRef = inject(DestroyRef);
 
   constructor(
     private route: ActivatedRoute,
@@ -53,46 +50,61 @@ export class PokeDetailViewComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     document.body.style.overflow = 'hidden';
-    this.routeSub = this.route.params.subscribe(params => {
-      this.game_index = +params['game_index'];
-
-      if (this.pokeDataService.$pokemonList) {
-        this.pokeList = this.pokeDataService.$pokemonList;
-        this.findPokemon();
-      } else {
-        this.firebaseSub = this.firebaseService.getPokemon().subscribe({
-          next: data => {
-            this.pokeList = data;
-            this.findPokemon();
-          },
-          error: err => {
-            console.log(Error)
-          }
-        });
-      }
-    });
+    this.loadPokemon();
   }
 
-  ngOnDestroy(): void {
-    this.routeSub?.unsubscribe();
-    this.firebaseSub?.unsubscribe();
+  loadPokemon() {
+    this.route.params
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(params => {
+        this.game_index = +params['game_index'];
+        this.name = params['name'];
+        if (this.pokeDataService.$pokemonList) {
+          this.pokeList = this.pokeDataService.$pokemonList;
+          this.findPokemon();
+        } else {
+          this.firebaseService.getPokemon()
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe({
+              next: data => {
+                this.pokeList = data;
+                this.findPokemon();
+              },
+              error: Error => {
+                console.log(Error)
+              }});
+        }
+      });
   }
 
-  findPokemon(): void {
+  findPokemon() {
     if (this.game_index && this.pokeList) {
       this.pokemon = this.pokeList.find(entry => entry.game_index === this.game_index);
-      if (!this.pokemon) {
-        this.router.navigateByUrl(`/pokedex/pokemon/1`)
+      
+      if (this.pokemon) {
+        this.name = this.pokemon.name;
+        this.router.navigate(['/pokedex/pokemon', this.game_index, this.name]);
+      } else {
+        this.navigateToStart();
       }
+    } else {
+      this.navigateToStart();
     }
+  }
 
-    if (this.game_index === 0) {
-      this.router.navigateByUrl(`/pokedex/pokemon/1`)
-    }
+  navigateToStart() {
+    this.router.navigate(['/pokedex']);
   }
 
   startScrolling(e: Event) {
     e.preventDefault();
     document.body.style.overflow = 'unset';
+  }
+
+  handleScrollToTop() {
+    const element = document.querySelector('.detail-view-container');
+    if (element) {
+      (element as HTMLElement).scrollTo({ top: 0, behavior: 'instant' });
+    }
   }
 }
