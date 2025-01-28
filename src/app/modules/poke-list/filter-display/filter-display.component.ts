8d6d3d47eec23cf28fefc470078f52efe95fe0cd
br4hm <haehnlein.alexander@gmx.de>
2025-01-28 21:39:41 +0100

    refactor | comment filter display

--- src/app/modules/poke-list/filter-display/filter-display.component.ts
+++ src/app/modules/poke-list/filter-display/filter-display.component.ts
@@ -1,27 +1,39 @@
 import {Component, effect} from '@angular/core';
 import {PokeDataService} from '../../../core/services/poke-data.service';
 import {Filter, FilterRange} from '../../../core/models/pokemon';
+import {MatBadge} from '@angular/material/badge';
+import {MatIcon} from '@angular/material/icon';
+import {MatTooltip} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-filter-display',
-  imports: [],
+  imports: [
+    MatBadge,
+    MatIcon,
+    MatTooltip
+  ],
   templateUrl: './filter-display.component.html',
   styleUrl: './filter-display.component.scss'
 })
+
+/**
+ * A class for display Pokémon filter.
+ */
 export class FilterDisplayComponent {
   initialFilterValues!: Filter;
   excludedTypes!: string[];
   filterSelection!: Filter;
+
   gen_1: boolean | undefined = true;
   gen_2: boolean | undefined = true;
 
   heightFilter: FilterRange = {}
   weightFilter: FilterRange = {}
 
-  heightFilterChange?: boolean = false;
-  weightFilterChange?: boolean = false;
+  heightFilterChange: boolean = false;
+  weightFilterChange: boolean = false;
 
-  anyFilterSet?: boolean = false;
+  anyFilterSet: boolean = false;
 
   constructor(private pokeDataService: PokeDataService) {
     effect(() => {
@@ -33,10 +45,9 @@ export class FilterDisplayComponent {
     });
   }
 
+  /** Sets the initial height and weight filter values. */
   setInitialValues() {
-    if (!this.initialFilterValues) {
-      return;
-    }
+    if (!this.initialFilterValues) return;
 
     this.heightFilter.min = this.initialFilterValues.heightRange.startValue;
     this.heightFilter.max = this.initialFilterValues.heightRange.endValue;
@@ -44,51 +55,52 @@ export class FilterDisplayComponent {
     this.weightFilter.max = this.initialFilterValues.weightRange.endValue;
   }
 
+  /** Updates the current filter values based on user selection. */
   setCurrentFilterValues() {
-    if (!this.filterSelection) {
-      return;
-    }
+    if (!this.filterSelection) return;
 
     this.heightFilter.currentStart = this.filterSelection.heightRange.startValue;
     this.heightFilter.currentEnd   = this.filterSelection.heightRange.endValue;
     this.weightFilter.currentStart = this.filterSelection.weightRange.startValue;
     this.weightFilter.currentEnd   = this.filterSelection.weightRange.endValue;
-    this.gen_1                     = this.filterSelection.generation.gen_1;
-    this.gen_2                     = this.filterSelection.generation.gen_2;
+    this.gen_1                     = this.filterSelection.generation['gen_1'];
+    this.gen_2                     = this.filterSelection.generation['gen_2'];
 
     this.checkChanges();
   }
 
+  /** Checks if any filter values have changed. */
   checkChanges() {
-    this.heightFilterChange = this.heightFilter.max != this.heightFilter.currentEnd ||
-      this.heightFilter.min != this.heightFilter.currentStart;
+    this.heightFilterChange =
+        this.heightFilter.max != this.heightFilter.currentEnd ||
+        this.heightFilter.min != this.heightFilter.currentStart;
 
-    this.weightFilterChange = this.weightFilter.max != this.weightFilter.currentEnd ||
-      this.weightFilter.min != this.weightFilter.currentStart;
+    this.weightFilterChange =
+        this.weightFilter.max != this.weightFilter.currentEnd ||
+        this.weightFilter.min != this.weightFilter.currentStart;
 
-    this.anyFilterSet = JSON.stringify(this.filterSelection) !== JSON.stringify(this.initialFilterValues);
+    this.anyFilterSet =
+        JSON.stringify(this.filterSelection) !== JSON.stringify(this.initialFilterValues);
   }
 
+  /**
+   * Resets a specific filter category to its initial values.
+   * @param {keyof Filter} category - The filter category to reset.
+   * @param {string} key - The key of the specific filter within the category.
+   */
   deleteFilter(category: keyof Filter, key: string) {
-    this.filterSelection = {...this.filterSelection};
+      this.filterSelection = {...this.filterSelection};
 
-    if (category === 'heightRange') {
-      this.filterSelection[category].startValue = this.heightFilter.min ?? 0;
-      this.filterSelection[category].endValue = this.heightFilter.max ?? 0;
-    } else if (category === 'weightRange') {
-      this.filterSelection[category].startValue = this.weightFilter.min ?? 0;
-      this.filterSelection[category].endValue = this.weightFilter.max ?? 0;
-    } else if (category === 'generation') {
-      this.filterSelection[category].gen_1 = true;
-      this.filterSelection[category].gen_2 = true;
-    } else {
-      this.filterSelection.types[key] = true;
+      if (category === 'heightRange') {
+        this.filterSelection[category].startValue = this.heightFilter.min ?? 0;
+        this.filterSelection[category].endValue = this.heightFilter.max ?? 0;
+      } else if (category === 'weightRange') {
+        this.filterSelection[category].startValue = this.weightFilter.min ?? 0;
+        this.filterSelection[category].endValue = this.weightFilter.max ?? 0;
+      } else {
+        this.filterSelection[category][key] = true;
+      }
+
+      this.pokeDataService.setFilterSelection(this.filterSelection);
     }
-
-    this.pokeDataService.setFilterSelection(this.filterSelection);
-    }
-
-
-
-
 }
