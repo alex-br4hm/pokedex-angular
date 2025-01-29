d2e1c2047929fdff549315e3dd51ee564e321464
br4hm <haehnlein.alexander@gmx.de>
2025-01-29 17:48:41 +0100

    clean-up interfaces

--- src/app/core/models/pokemon.ts
+++ src/app/core/models/pokemon.ts
@@ -1,62 +1,58 @@
 export interface PokemonData {
   name: string;
-  url: string;
-  id: number;
+  url:  string;
+  id:   number;
 }
 
 export interface Pokemon {
-  name: string;
-  name_en: string;
-  info_texts: string[];
-  types_ger: string[];
-  types_en: string[];
-  img_url: string;
-  small_img_url: string;
-  game_index: number;
-  species_url: string;
-  stats: Stats[];
-  weight: number;
-  height: number;
-  genera: string;
+  name:                string;
+  name_en:             string;
+  info_texts:          string[];
+  types_ger:           string[];
+  types_en:            string[];
+  img_url:             string;
+  small_img_url:       string;
+  game_index:          number;
+  species_url:         string;
+  stats:               Stats[];
+  weight:              number;
+  height:              number;
+  genera:              string;
   evolution_chain_url: string;
-  evolution_chain?: Evolution[];
+  evolution_chain?:    Evolution[];
 }
 
 export interface Stats {
-  name: string;
+  name:      string;
   base_stat: number;
-  value?: number;
+  value?:    number;
 }
 
 export interface Evolution {
-  game_index: number;
-  img_url: string;
-  name: string;
+  game_index:  number;
+  img_url:     string;
+  name:        string;
   evolves_to?: Evolution[];
 }
 
 export interface Filter {
-  types: {
-    [key: string]: boolean;
-  };
+  types: { [key: string]: boolean; };
   weightRange: {
     startValue: number;
-    endValue: number;
+    endValue:   number;
   };
   heightRange: {
     startValue: number;
-    endValue: number;
+    endValue:   number;
   };
-  generation: {
-    [key: string]: boolean;
-  };
+  generation: { [key: string]: boolean; };
 }
 
 export interface FilterRange {
-  max?: number;
-  min?: number;
+  max?:          number;
+  min?:          number;
   currentStart?: number;
-  currentEnd?: number;
+  currentEnd?:   number;
 }
 
 
