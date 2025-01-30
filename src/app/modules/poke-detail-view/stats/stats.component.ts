374516753f31dae83281dd34d7e99a93ca8f9aff
br4hm <haehnlein.alexander@gmx.de>
2025-01-30 23:31:46 +0100

    refactor stats (chart)

--- src/app/modules/poke-detail-view/stats/stats.component.ts
+++ src/app/modules/poke-detail-view/stats/stats.component.ts
@@ -20,24 +20,44 @@ import {StatNamePipe} from '../../../shared/utils/stat-name.pipe';
 export class StatsComponent implements OnInit, AfterViewInit {
   @Input() stats: Stats[] = [];
 
+  ctx!: HTMLCanvasElement;
+  labels!: string[];
+  data!: (number | undefined)[];
+
   constructor() {}
 
+  /**
+   * Registering all required chart.js elements.
+   */
   ngOnInit() {
     Chart.register(...registerables);
   }
 
+  /**
+   * Triggers the chart loading process after the view is fully initialized.
+   */
   ngAfterViewInit() {
     this.loadChart();
   }
 
+  /**
+   * Loads and initializes the radar chart using Chart.js.
+   * It sets the context, labels, and data for the chart, and configures the chart options.
+   */
   loadChart() {
-    const ctx = document.getElementById('radarChart') as HTMLCanvasElement;
-    const labels: string[] = ['HP', 'Angriff', 'Verteidig.', 'Sp. Angr.', 'Sp. Verteid.', 'Geschwindigk.'];
-    const data = this.stats.map(stat => stat.value);
+    this.ctx    = document.getElementById('radarChart') as HTMLCanvasElement;
+    this.labels = ['HP', 'Angriff', 'Verteidig.', 'Sp. Angr.', 'Sp. Verteid.', 'Geschwindigk.'];
+    this.data   = this.stats.map(stat => stat.value);
 
-    new Chart(ctx, {
+    new Chart(this.ctx, {
       type: 'radar',
-      data: { labels, datasets: [{ label: 'Stats', data }] },
+      data: {
+        labels: this.labels,
+        datasets: [{
+          label: 'Stats',
+          data: this.data
+        }]
+      },
       options: {
         responsive: true,
         scales: {
@@ -48,7 +68,6 @@ export class StatsComponent implements OnInit, AfterViewInit {
             grid: { color: 'rgba(255, 255, 255, 0.75)' },
             suggestedMin: 0,
             suggestedMax: 120,
-
           },
         },
         plugins: {
