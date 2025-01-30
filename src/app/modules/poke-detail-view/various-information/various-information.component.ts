e14c509c88b7486936984b69f8145419d9389540
br4hm <haehnlein.alexander@gmx.de>
2025-01-30 23:40:13 +0100

    refactor various information

--- src/app/modules/poke-detail-view/various-information/various-information.component.ts
+++ src/app/modules/poke-detail-view/various-information/various-information.component.ts
@@ -3,19 +3,21 @@ import {Pokemon, Stats} from '../../../core/models/pokemon';
 import {PokeHeightPipe} from '../../../shared/utils/poke-height.pipe';
 import {PokeWeightPipe} from '../../../shared/utils/poke-weight.pipe';
 import {PokeNumberPipePipe} from '../../../shared/utils/poke-number-pipe.pipe';
+import {
+  CustomLoadingSpinnerComponent
+} from '../../../shared/ui/custom-loading-spinner/custom-loading-spinner.component';
 
 @Component({
   selector: 'app-various-information',
   imports: [
     PokeHeightPipe,
     PokeWeightPipe,
-    PokeNumberPipePipe
+    PokeNumberPipePipe,
+    CustomLoadingSpinnerComponent
   ],
   templateUrl: './various-information.component.html',
   styleUrl: './various-information.component.scss'
 })
 export class VariousInformationComponent {
   @Input() pokemon: Pokemon | undefined;
-
-
 }
