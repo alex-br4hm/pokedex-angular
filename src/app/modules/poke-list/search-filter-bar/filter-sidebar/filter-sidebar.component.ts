24b73954571df69f525840ff74c0c49f13531a2b
br4hm <haehnlein.alexander@gmx.de>
2025-01-31 01:19:56 +0100

    refactor filter-sidebar

--- src/app/modules/poke-list/search-filter-bar/filter-sidebar/filter-sidebar.component.ts
+++ src/app/modules/poke-list/search-filter-bar/filter-sidebar/filter-sidebar.component.ts
@@ -29,51 +29,56 @@ export class FilterSidebarComponent implements OnInit {
   maxHeight: number  = 0;
   minWeight: number  = 0;
   maxWeight: number  = 0;
+  heights: number[]  = [];
+  weights: number[]  = [];
   isLoading: boolean = true;
 
   isFormChanged: boolean        = false;
   isInitialFormChanged: boolean = false;
   resetDialogOpen: boolean      = false;
-  sessionFilters?: any;
+  currentFilter?: Filter;
+  initialFilterValues!: Filter;
 
   @Input() filterToggled!: boolean;
   @Output() filterToggledChange = new EventEmitter<boolean>();
 
   _formBuilder = inject(FormBuilder);
 
-  initialFilterValues!: Filter;
+  constructor(private pokeDataService: PokeDataService) {}
 
-  constructor(private pokeDataService: PokeDataService) {
-  }
-
+  /**
+   * A FormGroup representing the filter selections for Pokémon.
+   * This includes selections for types, weight range, height range, and generation.
+   * Each group contains individual controls with their respective default values.
+   */
   filterSelections = this._formBuilder.group({
     types: this._formBuilder.group({
-      Normal: true,
-      Feuer: true,
-      Wasser: true,
+      Normal:  true,
+      Feuer:   true,
+      Wasser:  true,
       Elektro: true,
       Pflanze: true,
-      Flug: true,
-      Käfer: true,
-      Gift: true,
+      Flug:    true,
+      Käfer:   true,
+      Gift:    true,
       Gestein: true,
-      Boden: true,
+      Boden:   true,
       Kämpfer: true,
-      Eis: true,
-      Psycho: true,
-      Geist: true,
-      Drache: true,
-      Fee: true,
+      Eis:     true,
+      Psycho:  true,
+      Geist:   true,
+      Drache:  true,
+      Fee:     true,
       Unlicht: true,
-      Stahl: true,
+      Stahl:   true,
     }),
     weightRange: this._formBuilder.group({
       startValue: 0,
-      endValue: 0,
+      endValue:   0,
     }),
     heightRange: this._formBuilder.group({
       startValue: 0,
-      endValue: 0,
+      endValue:   0,
     }),
     generation: this._formBuilder.group({
       gen_1: true,
@@ -81,75 +86,90 @@ export class FilterSidebarComponent implements OnInit {
     })
   })
 
+  /**
+   * Fetches the Pokémon list, current filter, and initializes values.
+   * Sets the initial filter values if no current filter is available.
+   */
   ngOnInit() {
-    this.pokeList = this.pokeDataService.$pokemonList;
+    this.pokeList      = this.pokeDataService.$pokemonList;
+    this.currentFilter = this.pokeDataService.filterSelection();
     this.getMinMaxValues();
-    this.sessionFilters = this.pokeDataService.filterSelection();
     this.setInitialFilterSelection();
 
-    if (this.sessionFilters) {
-      this.filterSelections.patchValue(this.sessionFilters);
+    if (this.currentFilter) {
+      this.filterSelections.patchValue(this.currentFilter);
     } else {
       this.patchInitialFilterValues();
     }
 
     this.checkFormChanges();
     this.isLoading = false;
-
   }
 
+  /**
+   * Monitors changes in the form values and updates the change tracking flags.
+   * Checks if the form values are initially changed compared to the original filter values.
+   */
   checkFormChanges() {
-    if (this.sessionFilters) {
+    if (this.currentFilter) {
       this.isInitialFormChanged =
-        JSON.stringify(this.sessionFilters) !== JSON.stringify(this.initialFilterValues);
+        JSON.stringify(this.currentFilter) !== JSON.stringify(this.initialFilterValues);
     }
 
     this.filterSelections.valueChanges.subscribe(() => {
       this.isFormChanged =
-        JSON.stringify(this.sessionFilters) !== JSON.stringify(this.filterSelections.value);
+        JSON.stringify(this.pokeDataService.filterSelection()) !== JSON.stringify(this.filterSelections.value);
       this.isInitialFormChanged =
         JSON.stringify(this.filterSelections.value) !== JSON.stringify(this.initialFilterValues);
     });
   }
 
+  /**
+   * Calculates the minimum and maximum values for weight and height from the Pokémon list.
+   * These values are used to initialize the weight and height range filters.
+   */
   getMinMaxValues() {
-    const weights: number[] = this.pokeList.map(pokemon => pokemon.weight);
-    const heights: number[] = this.pokeList.map(pokemon => pokemon.height);
-    this.minWeight          = Math.min(...weights) / 10 ;
-    this.maxWeight          = Math.max(...weights) / 10;
-    this.minHeight          = Math.min(...heights) / 10;
-    this.maxHeight          = Math.max(...heights) / 10;
+    this.weights   = this.pokeList.map(pokemon => pokemon.weight);
+    this.heights   = this.pokeList.map(pokemon => pokemon.height);
+    this.minWeight = Math.min(...this.weights) / 10 ;
+    this.maxWeight = Math.max(...this.weights) / 10;
+    this.minHeight = Math.min(...this.heights) / 10;
+    this.maxHeight = Math.max(...this.heights) / 10;
   }
 
+  /**
+   * Sets the initial filter values for types, weight range, height range, and generation.
+   * These values are passed to the data service for use throughout the app.
+   */
   setInitialFilterSelection() {
     this.initialFilterValues = {
       types: {
-        Normal: true,
-        Feuer: true,
-        Wasser: true,
+        Normal:  true,
+        Feuer:   true,
+        Wasser:  true,
         Elektro: true,
         Pflanze: true,
-        Flug: true,
-        Käfer: true,
-        Gift: true,
+        Flug:    true,
+        Käfer:   true,
+        Gift:    true,
         Gestein: true,
-        Boden: true,
+        Boden:   true,
         Kämpfer: true,
-        Eis: true,
-        Psycho: true,
-        Geist: true,
-        Drache: true,
-        Fee: true,
+        Eis:     true,
+        Psycho:  true,
+        Geist:   true,
+        Drache:  true,
+        Fee:     true,
         Unlicht: true,
-        Stahl: true,
+        Stahl:   true,
       },
       weightRange: {
         startValue: this.minWeight,
-        endValue: this.maxWeight,
+        endValue:   this.maxWeight,
       },
       heightRange: {
         startValue: this.minHeight,
-        endValue: this.maxHeight,
+        endValue:   this.maxHeight,
       },
       generation: {
         gen_1: true,
@@ -160,16 +180,28 @@ export class FilterSidebarComponent implements OnInit {
     this.pokeDataService.setInitialFilterValues(this.initialFilterValues);
   }
 
+  /**
+   * Patches the filter selections with the initial filter values.
+   * This is used to reset the filter selections to their default state.
+   */
   patchInitialFilterValues() {
     this.filterSelections.patchValue(this.initialFilterValues);
-    this.sessionFilters = this.initialFilterValues;
+    this.currentFilter = this.initialFilterValues;
   }
 
+  /**
+   * Applies the current filter selection and updates the filter values in the data service.
+   * It also scrolls the page to the top after applying the filter.
+   */
   useFilterSelection() {
     document.body.style.overflow = 'unset';
     this.filterToggledChange.emit(false);
-    const filterSelections = JSON.stringify(this.filterSelections.value);
-    this.pokeDataService.setFilterSelection(JSON.parse(filterSelections));
+    const formattedFilterSelections = JSON.stringify(this.filterSelections.value);
+    this.pokeDataService.setFilterSelection(JSON.parse(formattedFilterSelections));
+    this.scrollToTop();
+  }
+
+  scrollToTop() {
     window.scroll({top: 0, behavior: 'smooth'});
   }
 
@@ -177,6 +209,9 @@ export class FilterSidebarComponent implements OnInit {
     this.resetDialogOpen = true;
   }
 
+  /**
+   * Resets the filter selections to their initial values and closes the reset dialog.
+   */
   resetFilter() {
     this.resetDialogOpen = false;
     this.isInitialFormChanged = false;
